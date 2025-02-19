454ba28b8068adc11564eadbc67fecf7f6aaf169
EiEiNisa <nisawara.ba@rmuti.ac.th>
2025-02-20 05:21:20 +0700

    Update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -224,8 +224,12 @@
     <!-- Image Slideshow -->
     <div class="slideshow-container py-3">
         @for ($i = 1; $i <= 6; $i++)
+            @php
+                // ดึงชื่อไฟล์จาก session หรือใช้ชื่อไฟล์ดีฟอลต์
+                $slidePath = session("slide_$i", "slide$i.png");
+            @endphp
             <div class="mySlides">
-                <img src="{{ asset('images/' . session("slide_$i", "slide$i.png")) }}" alt="Slide {{ $i }}">
+                <img src="{{ asset('images/' . $slidePath) }}" alt="Slide {{ $i }}">
             </div>
         @endfor
 
@@ -242,40 +246,39 @@
     </div>
 </div>
 
+<!-- JavaScript for Image Slideshow -->
+<script>
+    let slideIndex = 1;
+    showSlides(slideIndex);
 
-    
-    <!-- JavaScript for Image Slideshow -->
-    <script>
-        let slideIndex = 1;
-        showSlides(slideIndex);
-    
-        function plusSlides(n) {
-            showSlides(slideIndex += n);
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        let dots = document.getElementsByClassName("dot");
+        if (n > slides.length) { slideIndex = 1 }
+        if (n < 1) { slideIndex = slides.length }
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
         }
-    
-        function currentSlide(n) {
-            showSlides(slideIndex = n);
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
         }
-    
-        function showSlides(n) {
-            let i;
-            let slides = document.getElementsByClassName("mySlides");
-            let dots = document.getElementsByClassName("dot");
-            if (n > slides.length) { slideIndex = 1 }
-            if (n < 1) { slideIndex = slides.length }
-            for (i = 0; i < slides.length; i++) {
-                slides[i].style.display = "none";
-            }
-            for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-            }
-            slides[slideIndex - 1].style.display = "block";
-            dots[slideIndex - 1].className += " active";
-        }
-        setInterval(function() {
-            plusSlides(1);
-        }, 3000); // Change slide every 3 seconds
-    </script>
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+    }
+    setInterval(function() {
+        plusSlides(1);
+    }, 3000); // Change slide every 3 seconds
+</script>
+
 
     <!-- Article Slideshow -->
     <div class="article-slideshow-container py-3">
