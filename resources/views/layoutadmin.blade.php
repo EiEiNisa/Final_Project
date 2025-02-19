65e6ff92dfbcd2ad3328068ff00456ecce6ba655
EiEiNisa <nisawara.ba@rmuti.ac.th>
2025-02-19 07:04:13 +0700

    update

--- resources/views/layoutadmin.blade.php
+++ resources/views/layoutadmin.blade.php
@@ -10,7 +10,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Bootstrap JS (และ Popper.js สำหรับบางฟังก์ชัน) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JSxBa/OzP+ob0p8pbpk+2i6p5ZjLFhQ9TcF81ltCLNcIZ8dBdTPQ8Tka1R0zY6T2" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
