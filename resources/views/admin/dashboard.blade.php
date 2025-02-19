8239834201f5d47ddf3828eb751c819f7355b29b
EiEiNisa <nisawara.ba@rmuti.ac.th>
2025-02-19 09:32:59 +0700

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -61,7 +61,7 @@
     let diseaseChart = null; // ตัวแปรเก็บกราฟโรค
 
     async function fetchDashboardData() {
-        let response = await fetch("{{ route('dashboard.data') }}");
+        let response = await fetch("{{ route('dashboard.data', [], true) }}");
         if (!response.ok) {
             alert("ไม่สามารถดึงข้อมูลได้");
             return;
