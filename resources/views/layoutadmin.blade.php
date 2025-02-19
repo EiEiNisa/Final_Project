97fdc042148649a6f64cd321b806e36f5a001fd4
EiEiNisa <nisawara.ba@rmuti.ac.th>
2025-02-19 07:38:43 +0700

    update

--- resources/views/layoutadmin.blade.php
+++ resources/views/layoutadmin.blade.php
@@ -9,15 +9,14 @@
     <title>Thung Setthi Community</title>
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Font Awesome -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 
     <!-- Bootstrap JS (รวม Popper.js ในตัว) -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
-    </script>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</head>
 
     <style>
     body {
