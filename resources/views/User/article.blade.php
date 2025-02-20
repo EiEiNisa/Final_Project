85b41fb3a2e172bfd6743bbb7f95e7718b6be5f3
EiEiNisa <nisawara.ba@rmuti.ac.th>
2025-02-20 09:18:53 +0700

    Update

--- resources/views/User/article.blade.php
+++ resources/views/User/article.blade.php
@@ -61,7 +61,7 @@
     <div class="article-container">
         <h1 class="article-title">{{ $article->title }}</h1>
         <p class="article-meta">{{ $article->post_date }} | {{ $article->author }}</p>
-        <img src="{{ $article->image }}" class="card-img-top" alt="{{ $article->title }}">
+        <img src="{{ asset($article->image) }}" class="card-img-top" alt="{{ $article->title }}">
         <div class="article-content">
             {!! nl2br(e($article->description)) !!}
         </div>
