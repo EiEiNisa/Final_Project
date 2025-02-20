8dcabc491423629bda6c8d4974756c629beff22e
EiEiNisa <nisawara.ba@rmuti.ac.th>
2025-02-21 04:12:24 +0700

    adding files

--- resources/views/admin/editrecord.blade.php
+++ resources/views/admin/editrecord.blade.php
@@ -624,38 +624,6 @@ form {
                 });
                 </script>
 
-                <div class="custom-pagination mt-3">
-                    @if ($healthRecords->onFirstPage())
-                    <span class="disabled">ย้อนกลับ</span>
-                    @else
-                    <a href="{{ $healthRecords->previousPageUrl() }}">ย้อนกลับ</a>
-                    @endif
-
-                    <span>ทั้งหมด {{ $healthRecords->total() }} รายการ</span>
-
-                    @if ($healthRecords->hasMorePages())
-                    <a href="{{ $healthRecords->nextPageUrl() }}">ถัดไป</a>
-                    @else
-                    <span class="disabled">ถัดไป</span>
-                    @endif
-                </div>
-
-                <script>
-                document.addEventListener("DOMContentLoaded", function() {
-                    document.querySelectorAll(".accordion-button").forEach(button => {
-                        button.addEventListener("click", function() {
-                            let target = document.querySelector(this.getAttribute(
-                                "data-bs-target"));
-                            if (target.classList.contains("show")) {
-                                bootstrap.Collapse.getInstance(target).hide();
-                            } else {
-                                new bootstrap.Collapse(target).show();
-                            }
-                        });
-                    });
-                });
-                </script>
-
             </div>
         </form>
     </div>
