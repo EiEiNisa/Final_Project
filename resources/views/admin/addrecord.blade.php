9c97b92ab7ae2ad0e7ae9d23530edd7ee3ce59a9
EiEiNisa <nisawara.ba@rmuti.ac.th>
2025-03-02 19:19:43 +0700

    update_addrecord3/02

--- resources/views/admin/addrecord.blade.php
+++ resources/views/admin/addrecord.blade.php
@@ -439,21 +439,18 @@ form {
             });
             </script>
 
-            <div class="modal fade" id="infoModal" tabindex="-1" role="dialog" aria-labelledby="infoModalLabel"
-                aria-hidden="true">
-                <div class="modal-dialog" role="document">
+            <div class="modal fade" id="infoModal" tabindex="-1" aria-labelledby="infoModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="infoModalLabel">ข้อมูลในระบบ</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
                             ข้อมูลนี้มีอยู่ในระบบ
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">ปิด</button>
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ปิด</button>
                         </div>
                     </div>
                 </div>
--- resources/views/layoutadmin.blade.php
+++ resources/views/layoutadmin.blade.php
@@ -18,8 +18,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
 
     <style>
     body {
