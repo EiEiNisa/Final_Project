3d798e3f9fefca6d6673a06e46c24aa201b9db6b
EiEiNisa <nisawara.ba@rmuti.ac.th>
2025-03-02 19:14:35 +0700

    update3/02

--- resources/views/layoutadmin.blade.php
+++ resources/views/layoutadmin.blade.php
@@ -17,6 +17,10 @@
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+
     <style>
     body {
         background-color: #7DA7D8;
