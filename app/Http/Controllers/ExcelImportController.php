b2e728de153f7b4421aba2f2341bb2dc4103e6d6
EiEiNisa <nisawara.ba@rmuti.ac.th>
2025-03-04 18:13:19 +0700

    update_import04/03

--- app/Http/Controllers/ExcelImportController.php
+++ app/Http/Controllers/ExcelImportController.php
@@ -5,38 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Recorddata;
 use Maatwebsite\Excel\Facades\Excel;
-use Illuminate\Support\Facades\Validator;
+use App\Imports\DataImport; // เพิ่มบรรทัดนี้
 
 class ExcelImportController extends Controller
 {
     public function import(Request $request)
     {
-        $data = $request->input('data');
-
-        if (!$data || count($data) == 0) {
-            return response()->json(['message' => 'ไม่มีข้อมูลที่สามารถบันทึกได้'], 400);
-        }
+        $file = $request->file('file'); 
 
         try {
-            foreach ($data as $row) {
-                $recorddata = Recorddata::firstOrCreate([
-                    'id_card' => $row['id_card'],
-                    'prefix' => $row['prefix'],
-                    'name' => $row['name'],
-                    'surname' => $row['surname'],
-                    'housenumber' => $row['housenumber'],
-                    'birthdate' => $row['birthdate'],
-                    'age' => $row['age'],
-                    'blood_group' => $row['blood_group'],
-                    'weight' => $row['weight'],
-                    'height' => $row['height'],
-                    'waistline' => $row['waistline'],
-                    'bmi' => $row['bmi'],
-                    'phone' => $row['phone'],
-                    'idline' => $row['idline'],
-                    'user_id' => $row['user_id'],
-                ]);
-            }
+            Excel::import(new DataImport, $file); // เรียกใช้ DataImport
 
             return response()->json(['message' => 'นำเข้าข้อมูลสำเร็จ!'], 200);
         } catch (\Exception $e) {
