e94e20ab4ecdc2e6228bd7690a545bdf63d025ae
EiEiNisa <nisawara.ba@rmuti.ac.th>
2025-03-04 18:38:59 +0700

    update_import04/03

--- app/Http/Controllers/ExcelImportController.php
+++ app/Http/Controllers/ExcelImportController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Recorddata;
 use Maatwebsite\Excel\Facades\Excel;
 use App\Imports\DataImport; // เพิ่มบรรทัดนี้
+use Illuminate\Support\Facades\Log; // เพิ่มบรรทัดนี้
 
 class ExcelImportController extends Controller
 {
@@ -14,10 +15,12 @@ class ExcelImportController extends Controller
         $file = $request->file('file'); 
 
         try {
+            Log::info('File info:', ['file' => $file]); // เพิ่มบรรทัดนี้
             Excel::import(new DataImport, $file); // เรียกใช้ DataImport
 
             return response()->json(['message' => 'นำเข้าข้อมูลสำเร็จ!'], 200);
         } catch (\Exception $e) {
+            Log::error('Import error:', ['error' => $e->getMessage()]); // เพิ่มบรรทัดนี้
             return response()->json(['message' => 'เกิดข้อผิดพลาด: ' . $e->getMessage()], 500);
         }
     }
