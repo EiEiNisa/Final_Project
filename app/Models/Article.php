3fe1e3f27cd88fcba1fbfda73401fc16dacbad23
64332110104-1 <143884586+64332110104-1@users.noreply.github.com>
2025-03-06 04:21:51 +0700

    Article.php

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -9,5 +9,5 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model
 {
     protected $table = 'articles'; // ชื่อของตารางในฐานข้อมูล
-    protected $fillable = ['title', 'description', 'author', 'post_date', 'image'];
+    protected $fillable = ['title', 'description', 'author', 'post_date', 'image','video_link','video_upload'];
 }
