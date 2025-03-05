f883ba80663216d5f79d52a2bf5d26f9a90bc539
EiEiNisa <nisawara.ba@rmuti.ac.th>
2025-03-06 01:11:56 +0700

    update_editrecord06/03

--- resources/views/admin/editrecord.blade.php
+++ resources/views/admin/editrecord.blade.php
@@ -684,7 +684,7 @@ form {
                                     </div>
                                 </div>
                             </div>
-                            <a href="{{ route('recorddata.edit_general_information', ['recorddata_id' => $recorddata->id, 'checkup_id' => count($healthRecords) - $index]) }}"
+                            <a href="{{ route('recorddata.edit_general_information', ['recorddata_id' => $recorddata->id, 'checkup_id' => $checkup_id]) }}"
                                 class="btn btn-secondary" id="#editBtn">
                                 แก้ไขข้อมูล
                             </a>
