a930b3fc4b5b8acaaea413cca6e65c1d9dc2956b
EiEiNisa <nisawara.ba@rmuti.ac.th>
2025-03-06 20:18:23 +0700

    update_style_record06/03

--- resources/views/admin/record.blade.php
+++ resources/views/admin/record.blade.php
@@ -728,9 +728,10 @@ tbody {
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="alertModalLabel" style="color: red;">แจ้งเตือน</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                                style="font-size: 12px; width: 20px; height: 20px;"></button>
                         </div>
-                        <div class="modal-body" id="alertMessage">
+                        <div class="modal-body" id="alertMessage" style="font-size:15px;">
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ปิด</button>
