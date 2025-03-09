ea296b017132fcebb65f9d614dc934e2224d82a1
EiEiNisa <nisawara.ba@rmuti.ac.th>
2025-03-09 13:46:07 +0700

    update_edit_form_record_general08/03

--- app/Models/CustomFieldGeneral.php
+++ app/Models/CustomFieldGeneral.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class CustomFieldGeneral extends Model
 {
     use HasFactory;
+    protected $table = 'custom_field_general';
     protected $fillable = ['name', 'label', 'field_type', 'options'];
     
 }
