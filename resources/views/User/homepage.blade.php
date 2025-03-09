8bb46c374cfc96135b814a291a60c53ef973c29c
64332110104-1 <143884586+64332110104-1@users.noreply.github.com>
2025-03-10 00:56:56 +0700

    Update homepage.blade.php

--- resources/views/User/homepage.blade.php
+++ resources/views/User/homepage.blade.php
@@ -223,23 +223,30 @@
 <div class="container py-2">
     <!-- Image Slideshow -->
     <div class="slideshow-container py-3">
-        @foreach ($slides as $slide)
+        @for ($i = 1; $i <= 6; $i++)
+            @php
+                $slideImage = null;
+                foreach (['png', 'jpg', 'jpeg', 'webp'] as $ext) {
+                    if (file_exists(public_path("images/slide$i.$ext"))) {
+                        $slideImage = asset("images/slide$i.$ext");
+                        break;
+                    }
+                }
+                $slideImage = $slideImage ?? asset('images/default.png');
+            @endphp
+            
             <div class="mySlides">
-                <img src="{{ asset($slide->path) }}?t={{ time() }}" alt="Slide {{ $loop->iteration }}">
+                <img src="{{ $slideImage }}?t={{ time() }}" alt="Slide {{ $i }}">
             </div>
-        @endforeach
-
-        <!-- Next/Prev Buttons -->
-        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+        @endfor
     </div>
 
-    <!-- Dots -->
-    <div style="text-align:center">
-        @foreach ($slides as $index => $slide)
-            <span class="dot" onclick="currentSlide({{ $index + 1 }})"></span>
-        @endforeach
-    </div>
+
+<!-- Dots -->
+<div style="text-align:center">
+    @for ($i = 1; $i <= 6; $i++) <span class="dot" onclick="currentSlide({{ $i }})"></span>
+        @endfor
+</div>
 </div>
 
 <!-- JavaScript for Image Slideshow -->
