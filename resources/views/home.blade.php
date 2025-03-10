7299df12263f55ef6b33c75e980f2201c795fc80
64332110109-1 <143981599+EiEiNisa@users.noreply.github.com>
2025-03-10 11:08:46 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -297,7 +297,7 @@
                 @foreach($chunk as $article)
                     <div class="card">
                     <img src="{{ asset($article->image) }}" class="card-img-top" alt="{{ $article->title }}">
-                        <img src="{{ asset(json_decode($article->image)[0] ?? '') }}" alt="Article Image">
+                        <img src="{{ asset(json_decode($article->image,1)[0] ?? '') }}" alt="Article Image">
 
                     <div class="card-body">
                             <h5 class="card-title">{{ $article->title }}</h5>
